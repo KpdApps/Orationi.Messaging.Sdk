4c6a2bd2ddf640cd8a12dcb7091af78facdefc21
Denis Vostrikov <eagl@bmail.ru>
2018-09-25 16:51:21 +0300

    #4 - Move interface from Messaging.ServerCore

---
+++ KpdApps.Orationi.Messaging.Sdk/Workflow/IWorkflowExecutionContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+
+namespace KpdApps.Orationi.Messaging.Sdk.Workflow
+{
+    public interface IWorkflowExecutionContext
+    {
+        Guid MessageId { get; }
+
+        int RequestCode { get; }
+
+        string MessageBody { get; }
+
+        IDictionary GlobalSettings { get; }
+    }
+}
